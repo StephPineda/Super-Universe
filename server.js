3a66afe7bb7cfcf6f179027a650cb3849a43ecc5
Andy Weiss <andyweiss1982@gmail.com>
2019-05-07 14:07:31 -0400

    node env

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ const axios = require("axios")
 
 app.get('/bio/search/:query', (request, response) => {
   //axios.get(`https://superheroapi.com/${process.env.HERO_API_KEY}/search/${request.params.query}`)
-  axios.get(`https://superheroapi.com/api/1955038897933393/search/${request.params.query}`)
+  axios.get(`https://superheroapi.com/api/${process.env.API_KEY}/search/${request.params.query}`)
     .then(superResponse => response.json(superResponse.data.results || []))
 })
 
 app.get('/api/bio/:id', (request, response) => {
-  axios.get(`https://superheroapi.com/api/1955038897933393/${request.params.id}/biography`)
+  axios.get(`https://superheroapi.com/api/${process.env.API_KEY}/${request.params.id}/biography`)
     .then(superResponse => response.json(superResponse.data))
 })
 
 app.get('/api/image/:id', (request, response) => {
-  axios.get(`https://superheroapi.com/api/1955038897933393/${request.params.id}/image`)
+  axios.get(`https://superheroapi.com/api/${process.env.API_KEY}/${request.params.id}/image`)
     .then(superResponse => response.json(superResponse.data))
 })
 
